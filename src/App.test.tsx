af07d9041f2221e0ced3bc83feb9a2ba12ce4462
gcjuan <gcn.juan@gmail.com>
2023-05-04 14:45:34 +1200

    Update App.test.tsx

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -5,7 +5,7 @@ import App from "./App.js";
 describe('something truthy and falsy', () => {
     it('renders the correct text', () => {
         render(<App />);
-        const linkElement = screen.getByText('Vite + React');
+        const linkElement = screen.getByText('dsaddsadsadsa');
         expect(linkElement).toBeInTheDocument();
     });
 });
